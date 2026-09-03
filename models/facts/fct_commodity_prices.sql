e00b04e1dc1a409237c2fa60ea979bfe540197ef
Akbar Moledina <akbarmoledina@gmail.com>
2026-09-04 00:38:10 +0100

    added enforced data contracts on fact tables

--- models/facts/fct_commodity_prices.sql
+++ models/facts/fct_commodity_prices.sql
@@ -24,11 +24,14 @@ UNION ALL
 SELECT
     d.commodity_id,
     20231001 AS date_id,
-    avg_price_before_oct7 AS price,
+    s.avg_price_before_oct7 AS price,
     CURRENT_TIMESTAMP AS processed_date
-FROM {{ ref('stg_commodity_prices') }} s
+FROM (
+    SELECT DISTINCT commodity_name, unit_amount, avg_price_before_oct7
+    FROM {{ ref('stg_commodity_prices') }}
+    WHERE avg_price_before_oct7 IS NOT NULL
+) s
 INNER JOIN {{ ref('dim_commodity') }} d
     ON s.commodity_name = d.commodity_name
     AND s.unit_amount = d.unit_amount
-WHERE avg_price_before_oct7 IS NOT NULL
 {% endif %}
--- models/staging/stg_aid_received.sql
+++ models/staging/stg_aid_received.sql
@@ -3,13 +3,13 @@ WITH source AS (
 )
 
 SELECT
-    ID AS id,
-    "No. of Trucks" AS number_of_trucks,
+    ID::BIGINT AS id,
+    "No. of Trucks"::INT AS number_of_trucks,
     "Received Date"::DATE AS received_date,
     "Description of Cargo" AS items,
     COALESCE("Cargo Category", 'Unknown') AS cargo_category,
     Status AS status,
-    Quantity AS quantity,
+    Quantity::INT AS quantity,
     Units AS units,
     "Donating Country/ Organization" AS donated_by,
     COALESCE("Donation Type", 'Unknown') AS donation_type,
